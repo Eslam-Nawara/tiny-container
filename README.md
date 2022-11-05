a6d90e202514f92b606844210bb887a2fe17a7fc
Eslam-Nawara <eslamnawara44@gmail.com>
2022-11-05 18:06:52 +0200

    add implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# tiny-container
+# EslamNawara-Container
+Simple Container implementation in go using linux namespaces and cgroups.
+## Usage
+```sh
+$ git clone https://github.com/codescalersinternships/EslamNawara-Container
+$ cd EslamNawara-Container
+$ sudo run <command> <args>
+```
---
+++ container.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("Too few arguments")
+	}
+
+	switch os.Args[1] {
+	case "run":
+		run(os.Args[2:])
+	case "child":
+		child(os.Args[2], os.Args[3:])
+	default:
+		panic("invalid command")
+	}
+}
+
+func run(args []string) {
+	fmt.Println("Running as", os.Getpid())
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Credential: &syscall.Credential{
+			Uid: uint32(syscall.Getuid()),
+			Gid: uint32(syscall.Getgid()),
+		},
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Unshareflags: syscall.CLONE_NEWNS,
+		UidMappings:  []syscall.SysProcIDMap{{ContainerID: 0, HostID: syscall.Getuid(), Size: 1}},
+		GidMappings:  []syscall.SysProcIDMap{{ContainerID: 0, HostID: syscall.Getgid(), Size: 1}},
+	}
+
+	check(cmd.Run())
+}
+
+func child(command string, args []string) {
+	fmt.Println("Running as", os.Getpid())
+
+	newCgroup()
+
+	check(syscall.Sethostname(nameGenerator()))
+	check(syscall.Chroot("./rootfs"))
+	check(syscall.Chdir("/"))
+	check(syscall.Mount("proc", "proc", "proc", 0, ""))
+	check(syscall.Mount("dev", "dev", "tmpfs", 0, ""))
+
+	cmd := exec.Command(command, args...)
+
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	check(cmd.Run())
+	check(syscall.Unmount("/proc", 0))
+	check(syscall.Unmount("/dev", 0))
+}
+
+func newCgroup() {
+	pids := "/sys/fs/cgroup/pids"
+	os.MkdirAll(pids, 0755)
+
+	check(os.WriteFile(path.Join(pids, "pids.max"), []byte("10"), 0700))
+	check(os.WriteFile(path.Join(pids, "cgroup.procs"), []byte(strconv.Itoa(os.Getpid())), 0700))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
