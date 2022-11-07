3b3d197110b554b3e156e30f7aeebec192e851bd
Eslam-Nawara <eslamnawara44@gmail.com>
2022-11-07 15:33:15 +0200

    fix installation

--- cmd/container/install.sh
+++
@@ -1,6 +0,0 @@
-wget https://busybox.net/downloads/binaries/1.35.0-x86_64-linux-musl/busybox
-chmod a+x busybox
-mkdir -p  /tmp/rootfs/proc /tmp/rootfs/dev rootfs/bin
-./busybox --install rootfs/bin
-mv rootfs/bin /tmp/rootfs
-rm -r busybox rootfs
